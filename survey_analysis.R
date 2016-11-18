105cb3cd0eee25c1783deeb7b482cfdc9a3b75f0
jmoore523 <jmoore523@gmail.com>
2016-11-18 11:37:51 -0800

    graphs for BL

--- survey_analysis.R
+++ survey_analysis.R
@@ -6,6 +6,9 @@ library(ggplot2)
 survey_results <- read.csv("survey_real_results.csv")
 pmus_df <- read.csv("pop_dist.tsv", sep = "\t")
 
+########################
+# Demographic Analysis #
+########################
 # Data Munging
 survey_results <- filter(survey_results, Answer.Gender != "")
 
@@ -36,9 +39,94 @@ table(survey_results$Answer.Gender)
 table(survey_results$Answer.Education)
 
 ## Plots
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+theme_set(theme_bw())
+colors_for_plot <- gg_color_hue(4)
+
+# Age
+age_df <- group_by(joint_df, age) %>% summarize(pop_prop = sum(pop_prop), survey_prop = sum(survey_prop))
+age_df <- gather(age_df, source, proportion, pop_prop:survey_prop, factor_key=TRUE)
+ggplot(age_df, aes(x = age, y = proportion, fill = source, group = source)) +
+  geom_bar(stat = "Identity", position="dodge") +
+  labs(x = "Age", y = "Proportion", fill = "") +
+  theme(legend.position = "bottom") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_y_continuous(breaks = seq(0,.6, by = .1)) +
+  scale_fill_manual(values = colors_for_plot[3:4],
+                    breaks = c("pop_prop","survey_prop"), 
+                    labels = c("Population", "Survey"))
+ggsave("Age_Dist.pdf", width = 6, height = 5)
+
+# Gender
 gender_df <- group_by(joint_df, sex) %>% summarize(pop_prop = sum(pop_prop), survey_prop = sum(survey_prop))
 gender_df <- gather(gender_df, source, proportion, pop_prop:survey_prop, factor_key=TRUE)
 ggplot(gender_df, aes(x = sex, y = proportion, fill = source, group = source)) +
   geom_bar(stat = "Identity", position="dodge") +
-  labs(x = "Sex", y = "Proportion") +
-  
+  labs(x = "Sex", y = "Proportion", fill = "") +
+  theme(legend.position = "bottom") +
+  scale_y_continuous(breaks = seq(0,.6, by = .1)) +
+  scale_x_discrete(breaks = c("male","female"),
+                  labels = c("Male","Female")) +
+  scale_fill_manual(values = colors_for_plot[3:4],
+                    breaks = c("pop_prop","survey_prop"), 
+                    labels = c("Population", "Survey"))
+ggsave("Gender_Dist.pdf", width = 6, height = 5)
+
+# Race
+race_df <- group_by(joint_df, race) %>% summarize(pop_prop = sum(pop_prop), survey_prop = sum(survey_prop))
+race_df <- gather(race_df, source, proportion, pop_prop:survey_prop, factor_key=TRUE)
+ggplot(race_df, aes(x = race, y = proportion, fill = source, group = source)) +
+  geom_bar(stat = "Identity", position="dodge") +
+  labs(x = "Race", y = "Proportion", fill = "") +
+  theme(legend.position = "bottom") +
+  scale_y_continuous(breaks = seq(0,.8, by = .1)) +
+  scale_x_discrete(breaks = c("black","hispanic","other","white"),
+                   labels = c("Black","Hispanic","Other","White")) +
+  scale_fill_manual(values = colors_for_plot[3:4],
+                    breaks = c("pop_prop","survey_prop"), 
+                    labels = c("Population", "Survey"))
+ggsave("Race_Dist.pdf", width = 6, height = 5)
+
+# Education
+education_df <- group_by(joint_df, education) %>% summarize(pop_prop = sum(pop_prop), survey_prop = sum(survey_prop))
+education_df <- gather(education_df, source, proportion, pop_prop:survey_prop, factor_key=TRUE)
+education_df$education <- factor(education_df$education, 
+                                 levels = c("no high school diploma","high school graduate","college degree","postgraduate degree"), 
+                                 labels = c("No HS Diploma","HS Graduate","College Grad","Post-Grad"), ordered=TRUE)
+ggplot(education_df, aes(x = education, y = proportion, fill = source, group = source)) +
+  geom_bar(stat = "Identity", position="dodge") +
+  labs(x = "Education", y = "Proportion", fill = "") +
+  theme(legend.position = "bottom") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_y_continuous(breaks = seq(0,.6, by = .1)) +
+  scale_fill_manual(values = colors_for_plot[3:4],
+                    breaks = c("pop_prop","survey_prop"), 
+                    labels = c("Population", "Survey"))
+ggsave("Education_Dist.pdf", width = 6, height = 5)
+
+###############################
+# Unadjusted Answers Analysis #
+###############################
+# Number of responses in category
+table(survey_results$Answer.Political)
+table(survey_results$Answer.Automation)
+table(survey_results$Answer.Universal)
+
+prop.table(table(survey_results$Answer.Education, survey_results$Answer.Political),1)
+
+# Automation answer by demo
+prop.table(table(survey_results$Answer.Age, survey_results$Answer.Automation),1)
+prop.table(table(survey_results$Answer.Race, survey_results$Answer.Automation),1)
+prop.table(table(survey_results$Answer.Gender, survey_results$Answer.Automation),1)
+prop.table(table(survey_results$Answer.Education, survey_results$Answer.Automation),1)
+prop.table(table(survey_results$Answer.Political, survey_results$Answer.Automation),1)
+
+# UBI by demo
+prop.table(table(survey_results$Answer.Age, survey_results$Answer.Universal),1)
+prop.table(table(survey_results$Answer.Race, survey_results$Answer.Universal),1)
+prop.table(table(survey_results$Answer.Gender, survey_results$Answer.Universal),1)
+prop.table(table(survey_results$Answer.Education, survey_results$Answer.Universal),1)
+prop.table(table(survey_results$Answer.Political, survey_results$Answer.Universal),1)
